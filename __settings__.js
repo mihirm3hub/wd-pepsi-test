ae38718223655133ec08375a3a69f3e9e974b170
mihir <mihirmainkarm3@gmail.com>
2024-07-03 16:02:27 +0530

    WD:Upload project files

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1934475.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl1`, `webgl2`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 175207477, 163979655, 163979995, 163980011, 163980367, 164162624, 170370120, 164358372, 165113206, 169352658, 174653758, 164014524 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
